a96befc938d578946e1bde86252c3cf6c14caf65
AniPortal.live | Dev <nanunema123@gmail.com>
2025-10-11 03:24:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 const series = [
   {
     name: "Solo Leveling",
-    thumbnail: "https://i.postimg.cc/POSTIMAGE-THUMBNAIL.png",
+    thumbnail: "https://i.postimg.cc/N0vrWV2F/1696342675788-VKzxw.webp",
     seasons: [
       {
         name: "Staffel 1",
         episodes: [
-          { title: "Ep | 1", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 2", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 3", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 4", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 5", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 6", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 7", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 8", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 9", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 10", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 11", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 12", stream: "https://streamtape.com/e/STREAM-LINK-HIER" }
+          { title: "Ep | 1", stream: "https://streamtape.com/v/BaOWq6Mx7lUy1J6/Solo-Leveling-Ger-Dub-E01.mp4" },
+          { title: "Ep | 2", stream: "https://streamtape.com/v/dqLzAZwyPLfQoA/Solo-Leveling-Ger-Dub-E02.mp4" },
+          { title: "Ep | 3", stream: "https://streamtape.com/v/wyLDJZQAGyHoex/Solo-Leveling-Ger-Dub-E03.mp4" },
+          { title: "Ep | 4", stream: "https://streamtape.com/v/GQM4k8yrqPs174v/Solo-Leveling-Ger-Dub-E04.mp4" },
+          { title: "Ep | 5", stream: "https://streamtape.com/v/7zwdovpbv4fJM2/Solo-Leveling-Ger-Dub-E05.mp4" },
+          { title: "Ep | 6", stream: "https://streamtape.com/v/kvj7GPeAXesOGRO/Solo-Leveling-Ger-Dub-E06.mp4" },
+          { title: "Ep | 7", stream: "https://streamtape.com/v/zXjkaYzMObHX9P/Solo-Leveling-Ger-Dub-E07.mp4" },
+          { title: "Ep | 8", stream: "https://streamtape.com/v/jlGpLOZxyrSzXKl/Solo-Leveling-Ger-Dub-E08.mp4" },
+          { title: "Ep | 9", stream: "https://streamtape.com/v/RgMrRvw1YkcdWbX/Solo-Leveling-Ger-Dub-E09.mp4" },
+          { title: "Ep | 10", stream: "https://streamtape.com/v/bGodZgKG8LFP0Qq/Solo-Leveling-Ger-Dub-E10.mp4" },
+          { title: "Ep | 11", stream: "https://streamtape.com/v/XBAggZbqrYUB2e/Solo-Leveling-Ger-Dub-E11.mp4" },
+          { title: "Ep | 12", stream: "https://streamtape.com/v/3VoDWy4BAdhdg60/Solo-Leveling-Ger-Dub-E12.mp4" }
         ]
       },
       {
