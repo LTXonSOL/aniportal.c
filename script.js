9d6705733b8dc236af16de44452819a2a357e600
AniPortal.live | Dev <nanunema123@gmail.com>
2025-10-11 02:50:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const series = [
   {
-    name: "Serie 1",
+    name: "Solo Leveling",
     thumbnail: "https://i.postimg.cc/POSTIMAGE-THUMBNAIL.png",
     seasons: [
       {
@@ -17,15 +17,7 @@ const series = [
           { title: "Ep | 9", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
           { title: "Ep | 10", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
           { title: "Ep | 11", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 12", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 13", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 14", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 15", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 16", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 17", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 18", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 19", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 20", stream: "https://streamtape.com/e/STREAM-LINK-HIER" }
+          { title: "Ep | 12", stream: "https://streamtape.com/e/STREAM-LINK-HIER" }
         ]
       },
       {
@@ -50,32 +42,7 @@ const series = [
           { title: "Ep | 17", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
           { title: "Ep | 18", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
           { title: "Ep | 19", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 20", stream: "https://streamtape.com/e/STREAM-LINK-HIER" }
-        ]
-      },
-      {
-        name: "Staffel 3",
-        episodes: [
-          { title: "Ep | 1", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 2", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 3", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 4", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 5", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 6", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 7", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 8", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 9", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 10", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 11", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 12", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 13", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 14", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 15", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 16", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 17", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 18", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 19", stream: "https://streamtape.com/e/STREAM-LINK-HIER" },
-          { title: "Ep | 20", stream: "https://streamtape.com/e/STREAM-LINK-HIER" }
+          { title: "Ep | 20", stream: "https://streamtape.com/e/STREAM-LINK-HIER" } 
         ]
       }
     ]
